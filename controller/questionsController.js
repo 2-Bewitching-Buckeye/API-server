6422a39ae6376f05d904e5f72a5685e25a5c41c9
Patrick McConnell <pmcconnell13@gmail.com>
2021-03-22 21:15:49 -0700

    Got all API routes set

--- controller/questionsController.js
+++ controller/questionsController.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 //questions controllers
 exports.questionsGetController = (req, res) => {
-  console.log(req.body)
-  axios.get(`http://localhost:3000/qa/questions`, {params: req.body})
+  var query = req.query;
+  axios.get(`http://localhost:3000/qa/questions/?product_id=${query.product_id}&count=${query.count}&page=${query.page}`)
     .then((result) => {
       console.log(result.data)
       res.send(result.data)
@@ -14,25 +14,10 @@ exports.questionsGetController = (req, res) => {
     })
 }
 
-// exports.questionsPostController = (req, res) => {
-//   axios.get(`http://localhost:3000/qa/questions`)
-//     .then((result) => {
-//       console.log(result)
-//       res.send(result)
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//       res.sendStatus(400)
-//     })
-//   }
-// exports.questionsHelpfulController
-// exports.questionsReportController
-
-
-// //answers controllers
-exports.answerGetController = (req, res) => {
-  console.log(req.params.question_id)
-  axios.get(`http://localhost:3000/qa/questions/${req.params.question_id}/answers`)
+//throwing Converting circular structure to JSON error despite successful post
+exports.questionsPostController = (req, res) => {
+  var body = req.body;
+  axios.post(`http://localhost:3000/qa/questions`, body)
     .then((result) => {
       console.log(result.data)
       res.send(result.data)
@@ -42,6 +27,87 @@ exports.answerGetController = (req, res) => {
       res.sendStatus(400)
     })
   }
-// exports.answerPostController
-// exports.answerHelpfulController
-// exports.answerReportController
+
+  //throwing Converting circular structure to JSON error despite successful post
+exports.questionsHelpfulController = (req, res) => {
+var params = req.params;
+console.log(req.params)
+  axios.put(`http://localhost:3000/qa/questions/${params.question_id}/helpful`)
+    .then((result) => {
+      console.log(result.data)
+      res.send(result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+  }
+
+exports.questionsReportController = (req, res) => {
+var params = req.params;
+console.log(req.params)
+  axios.put(`http://localhost:3000/qa/questions/${params.question_id}/report`)
+    .then((result) => {
+      console.log(result.data)
+      res.send(result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+  }
+
+// //answers controllers
+exports.answerGetController = (req, res) => {
+  axios.get(`http://localhost:3000/qa/questions/${req.params.question_id}/answers`)
+    .then((result) => {
+      res.send(result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+  }
+
+ exports.answerPostController = (req, res) => {
+  var body = req.body;
+  console.log(body)
+  console.log(req.params)
+  axios.post(`http://localhost:3000/qa/questions/${req.params.question_id}/answers`, body)
+    .then((result) => {
+      console.log(result.data)
+      res.send(result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+}
+
+exports.answerHelpfulController = (req, res) => {
+var params = req.params;
+console.log(req.params)
+  axios.put(`http://localhost:3000/qa/answers/${params.answer_id}/helpful`)
+    .then((result) => {
+      console.log(result.data)
+      res.send(result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+  }
+
+exports.answerReportController = (req, res) => {
+  var params = req.params;
+console.log(req.params)
+  axios.put(`http://localhost:3000/qa/answers/${params.answer_id}/report`)
+    .then((result) => {
+      console.log(result.data)
+      res.send(result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+}
--- routes/questionsRouter.js
+++ routes/questionsRouter.js
@@ -7,20 +7,20 @@ const router = express.Router();
 //questions routes
 router.get('/questions', questionsGetController);
 
-// router.post('/questions', questionsPostController);
+router.post('/questions', questionsPostController);
 
-// router.put('/questions/:question_id/helpful', questionsHelpfulController);
+router.put('/questions/:question_id/helpful', questionsHelpfulController);
 
-// router.put('/questions/:question_id/report', questionsReportController);
+router.put('/questions/:question_id/report', questionsReportController);
 
 
 //answers routes
 router.get('/questions/:question_id/answers', answerGetController);
 
-// router.post('/questions/:question_id/answers', answerPostController);
+router.post('/questions/:question_id/answers', answerPostController);
 
-// router.put('/answers/:answer_id/helpful', answerHelpfulController);
+router.put('/answers/:answer_id/helpful', answerHelpfulController);
 
-// router.put('/answers/:answer_id/report', answerReportController);
+router.put('/answers/:answer_id/report', answerReportController);
 
 module.exports = router
